139e558bb464b1ea9d34e97d8cecd97b7b667e34
rzlwbo1 <rizalwb2@gmail.com>
2021-06-28 13:43:15 +0700

    simple date by rizal wibowo

---
+++ date.js
@@ -0,0 +1,51 @@
+// get date
+const date = new Date;
+
+      
+//get tanggal
+const currentDate = date.getDate();
+
+// get bulan 
+const months = [
+  'Januari',
+  'Februari',
+  'Maret',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Agustus',
+  'September',
+  'Oktober',
+  'November',
+  'Desember'
+];
+
+const month = months.filter((month, index) => index == (date.getMonth()));
+
+//get hari
+const days = ['minggu','senin', 'selasa', 'rabu', 'kamis', 'jum\'at', 'sabtu',];
+const day = days.filter((day, index) => index == (date.getDay()));
+
+// get tahun
+const year = date.getFullYear();
+
+// get jam and menit
+const hour = date.getHours();
+const minutes = `${date.getMinutes < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`}`;
+let convertMinutes = Number(minutes);
+
+// combined
+const dateNow = `Hari ${day} tanggal ${currentDate} ${month} ${year} jam ${hour}:${convertMinutes} `;
+
+// log
+console.log(dateNow);
+
+
+
+/// display to dom
+
+// get elem
+const dateElem = document.getElementById('tgl-hr');
+//fill and append
+dateElem.innerHTML = dateNow;
